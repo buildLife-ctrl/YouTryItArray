44823a5b2aacdf3395eeb1155d49467c941d5b94
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-08 08:59:06 -0500

    ini

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int inputAmount;
+        String word;
+
+        System.out.println("How many words will you put in?");
+        inputAmount = scan.nextInt();
+
+        String [] words = new String[inputAmount];
+        System.out.println("Enter " + inputAmount + " word(s):");
+        for (int i = 0; i < inputAmount; i++) {
+            words[i] = scan.nextLine();
+        }
+
+        String shortestWord = words[0];
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() < shortestWord.length()) {
+                shortestWord = words[i];
+            }
+        }
+    }
 }
