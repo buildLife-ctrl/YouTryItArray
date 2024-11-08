4ecc98f7bd2500e9d0cbae0920969cfcb2ac5634
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-08 09:32:17 -0500

    2ini

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,14 @@ public class Main {
 
         String [] words = new String[inputAmount];
         System.out.println("Enter " + inputAmount + " word(s):");
-        for (int i = 0; i < inputAmount; i++) {
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(i);
             words[i] = scan.nextLine();
         }
 
         String shortestWord = words[0];
         for (int i = 1; i < words.length; i++) {
+            System.out.println("line 21 "+i);
             if (words[i].length() < shortestWord.length()) {
                 shortestWord = words[i];
             }
