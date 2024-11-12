a0b5155a85b0fe58c4375ecede92d817db476666
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-12 08:31:04 -0500

    2ini

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int inputAmount;
-        String word;
 
+        //user input
         System.out.println("How many words will you put in?");
         inputAmount = scan.nextInt();
+        scan.nextLine();
 
+        //takes into account the words the user want us to print
         String [] words = new String[inputAmount];
         System.out.println("Enter " + inputAmount + " word(s):");
         for (int i = 0; i < words.length; i++) {
-            System.out.println(i);
             words[i] = scan.nextLine();
         }
 
+        //finds the shortest word in the inputted section
         String shortestWord = words[0];
-        for (int i = 1; i <= words.length; i++) {
-            System.out.println("line 21 "+i);
+        for (int i = 1; i < words.length; i++) {
             if (words[i].length() < shortestWord.length()) {
                 shortestWord = words[i];
             }
         }
+        System.out.println("The shortest word is: " + shortestWord);
     }
 }
